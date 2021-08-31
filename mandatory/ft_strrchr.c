ba6b53cbbf2d49e6ed56054ab4c6052909e7bec0
ayoub0x1 <ayoub0x1@wearehackerone.com>
2021-08-31 12:30:43 +0100

    libft

---
+++ mandatory/ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aymoulou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/16 14:42:14 by aymouloua           #+#    #+#           */
+/*   Updated: 2021/08/16 14:55:02 by macbook          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int		i;
+	char	*str;
+
+	str = (char *)s;
+	i = ft_strlen(str);
+	while (i != -1)
+	{
+		if (str[i] == (char)c)
+			return (str + i);
+		i--;
+	}
+	return (NULL);
+}
